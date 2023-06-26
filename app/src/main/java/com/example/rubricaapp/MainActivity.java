e1d1e4b63ea50336e751f71ad298dac5be468795
Stefano <stefanobrem@gmail.com>
2023-06-26 07:43:26 +0200

    Added new icon - Write on the sd card - Modify add

--- app/src/main/java/com/example/rubricaapp/MainActivity.java
+++ app/src/main/java/com/example/rubricaapp/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.rubricaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.storage.StorageManager;
+import android.os.storage.StorageVolume;
 import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.rubricaapp.databinding.ActivityMainBinding;
 import com.google.android.material.snackbar.Snackbar;
@@ -22,6 +29,7 @@ import java.io.InputStreamReader;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
@@ -29,8 +37,7 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
 
     private static String _FILENAME = "rubrica.txt";
-    private static String _SDPATH = Environment.getExternalStorageDirectory().getAbsolutePath();
-    private File _FILE = new File(_SDPATH + "/" + File.separator + _FILENAME);
+    private File _FILE = new File("");
 
     public ArrayList<Rubrica> _rubricaArrayList = new ArrayList<>();
 
@@ -47,13 +54,12 @@ public class MainActivity extends AppCompatActivity {
 
         Button nuovoDato = findViewById(R.id.nuovoDatoButton);
         nuovoDato.setOnClickListener(view -> aggiungiElemento());
-
     }
 
 
     public void aggiungiElemento() {
         Intent intent = new Intent(getBaseContext(), ModifyElement.class);
-        intent.putExtra("daModificare",true);
+        intent.putExtra("daModificare",false);
 
         startActivity(intent);
 
@@ -63,7 +69,17 @@ public class MainActivity extends AppCompatActivity {
 
     public void modificaElemento(View view) {
         Intent intent = new Intent(this,  ModifyElement.class);
-        intent.putExtra("_DAMODIFICARE", true);
+        intent.putExtra("daModificare", true);
+
+        String codice = ((TextView) view.findViewById(R.id.listCodice)).getText().toString();
+        String nome = ((TextView) view.findViewById(R.id.listNome)).getText().toString();
+        String telefono = ((TextView) view.findViewById(R.id.listTelefono)).getText().toString();
+        String note = ((TextView) view.findViewById(R.id.listNote)).getText().toString();
+
+        intent.putExtra("codice", codice);
+        intent.putExtra("nome", nome);
+        intent.putExtra("telefono", telefono);
+        intent.putExtra("note", note);
 
         startActivity(intent);
 
@@ -82,18 +98,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void askForPermissions() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R)
+        {
             if (!Environment.isExternalStorageManager()) {
                 Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
                 startActivity(intent);
                 return;
             }
+
+            StorageManager storageManager = (StorageManager) getSystemService(STORAGE_SERVICE);
+            List<StorageVolume> storageVolumeList = storageManager.getStorageVolumes();
+            StorageVolume storageVolume = storageVolumeList.get(1); // 1 is for external SD Card. 0 is for internal memory
+            _FILE = new File(storageVolume.getDirectory().getAbsolutePath() +"/"+ _FILENAME);
+
             createFile();
         }
     }
 
     public void createFile() {
-        if (!_FILE.exists()) {
+        if (!_FILE.exists())
+        {
             try {
                 _FILE.createNewFile();
             } catch (Exception e) {
@@ -127,9 +152,11 @@ public class MainActivity extends AppCompatActivity {
                         for(int i = 0; i < data.length; i++){
                             data[i] = data[i].replace('§', '\n');
                         }
-                        
-                        Rubrica rubrica = new Rubrica(data[0],data[1],data[2],data[3]);
-                        _rubricaArrayList.add(rubrica);
+
+                        if(data.length == 4) {
+                            Rubrica rubrica = new Rubrica(data[0], data[1], data[2], data[3]);
+                            _rubricaArrayList.add(rubrica);
+                        }
                     }
                 }
 
@@ -144,5 +171,4 @@ public class MainActivity extends AppCompatActivity {
         RubricaListAdapter adapter = new RubricaListAdapter(this, R.layout.adapter_view_layout, this._rubricaArrayList);
         mListView.setAdapter(adapter);
     }
-
 }
--- app/src/main/java/com/example/rubricaapp/ModifyElement.java
+++ app/src/main/java/com/example/rubricaapp/ModifyElement.java
@@ -2,29 +2,37 @@ package com.example.rubricaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.storage.StorageManager;
+import android.os.storage.StorageVolume;
+import android.provider.Settings;
 import android.view.WindowManager;
 
 import com.example.rubricaapp.databinding.ActivityModifyElementsBinding;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.io.Writer;
+import java.util.List;
 
 public class ModifyElement extends AppCompatActivity {
     private ActivityModifyElementsBinding binding;
 
     private static String _FILENAME = "rubrica.txt";
-    private static String _SDPATH = Environment.getExternalStorageDirectory().getAbsolutePath();
-    private File _FILE = new File(_SDPATH + "/" + File.separator + _FILENAME);
+    private File _FILE = new File("");
 
     private Boolean _daModificare;
-    private int _index;
-
+    private String _oldValue;
 
     @Override
     protected void onCreate(Bundle bundle) {
@@ -41,39 +49,93 @@ public class ModifyElement extends AppCompatActivity {
         binding.deleteButton.setOnClickListener(view -> cancellaElemento());
     }
 
-
     public void salvaElemento() {
         String codice = binding.codiceInput.getText().toString();
         String nome = binding.nomeInput.getText().toString();
         String telefono = binding.telefonoInput.getText().toString();
         String note = binding.noteInput.getText().toString();
 
-        //codice|nome|telefono|note
+        // codice|nome|telefono|note
 
-        if (codice.isEmpty() == true || nome.isEmpty() == true || telefono.isEmpty() == true || note.isEmpty() == true) {
-            Snackbar.make(findViewById(R.id.relativeLayout), "RIEMPIRE TUTTI I CAMPI!", Snackbar.LENGTH_LONG).show();
-        }
-        else
-        {
-            codice = codice.replaceAll("\n", "§");
-            nome = nome.replaceAll("\n", "§");
-            telefono = telefono.replaceAll("\n", "§");
-            note = note.replaceAll("\n", "§");
+        codice = codice.replaceAll("\n", "§");
+        nome = nome.replaceAll("\n", "§");
+        telefono = telefono.replaceAll("\n", "§");
+        note = note.replaceAll("\n", "§");
 
+        String whatToWrite = codice + "|" + nome + "|" + telefono + "|" + note + "|";
 
-            String whatToWrite = codice + "|" + nome + "|" + telefono + "|" + note + "|";
+        if (this._daModificare == true) {
+            // prendi l'id dell'elemento nella lista e modificalo
 
-            if (false) {
-                //prendi l'id dell'elemento nella lista e modificalo
-            }
-            else
-            {
-                try {
+            try {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    FileInputStream fileInput = new FileInputStream(_FILE);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(fileInput));
+
                     OutputStream outputStream = new FileOutputStream(_FILE, true);
                     Writer writer = new OutputStreamWriter(outputStream, "UTF-8");
 
-                    writer.append(whatToWrite);
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        line = line.replace(_oldValue, whatToWrite);
+                        writer.append(line);
+                    }
 
+                    reader.close();
+                    writer.flush();
+                    outputStream.flush();
+
+                    writer.close();
+                    outputStream.close();
+
+                    finish();
+                }
+            } catch (Exception e) {
+                Snackbar.make(findViewById(R.id.relativeLayout), "Errore nella lettura del file!", Snackbar.LENGTH_LONG)
+                        .show();
+            }
+
+        } else {
+            try {
+                OutputStream outputStream = new FileOutputStream(_FILE, true);
+                Writer writer = new OutputStreamWriter(outputStream, "UTF-8");
+
+                writer.append("\n");
+                writer.append(whatToWrite);
+
+                writer.flush();
+                outputStream.flush();
+
+                writer.close();
+                outputStream.close();
+
+                finish();
+            } catch (Exception e) {
+                Snackbar.make(findViewById(R.id.relativeLayout), "La scrittura del file è andata male!",
+                        Snackbar.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void cancellaElemento() {
+
+        if (this._daModificare == true) {
+
+            try {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    FileInputStream fileInput = new FileInputStream(_FILE);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(fileInput));
+
+                    OutputStream outputStream = new FileOutputStream(_FILE, true);
+                    Writer writer = new OutputStreamWriter(outputStream, "UTF-8");
+
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        line = "";
+                        writer.write(line);
+                    }
+
+                    reader.close();
                     writer.flush();
                     outputStream.flush();
 
@@ -81,37 +143,49 @@ public class ModifyElement extends AppCompatActivity {
                     outputStream.close();
 
                     exit();
-                } catch (Exception e) {
-                    Snackbar.make(findViewById(R.id.relativeLayout), "La scrittura del file è andata male!", Snackbar.LENGTH_LONG).show();
                 }
+            } catch (Exception e) {
+                Snackbar.make(findViewById(R.id.relativeLayout), "Errore nella lettura del file!", Snackbar.LENGTH_LONG)
+                        .show();
             }
+
         }
+
+        finish();
     }
-
-    public void cancellaElemento() {
-
-        exit();
-    }
-
 
     public void exit() {
         finish();
     }
 
-    private void searchElement(){
+    private void searchElement() {
 
     }
 
     private void loadElements() {
-        this._daModificare = getIntent().getBooleanExtra("daModificare",false);
+        this._daModificare = getIntent().getBooleanExtra("daModificare", false);
 
-        if(this._daModificare == true) {
+        if (this._daModificare == true) {
             searchElement();
 
-            binding.codiceInput.setText("");
-            binding.nomeInput.setText("");
-            binding.telefonoInput.setText("");
-            binding.noteInput.setText("");
+            binding.codiceInput.setText(getIntent().getStringExtra("codice"));
+            binding.nomeInput.setText(getIntent().getStringExtra("nome"));
+            binding.telefonoInput.setText(getIntent().getStringExtra("telefono"));
+            binding.noteInput.setText(getIntent().getStringExtra("note"));
+
+            String codice = binding.codiceInput.getText().toString().replaceAll("\n", "§");
+            String nome = binding.nomeInput.getText().toString().replaceAll("\n", "§");
+            String telefono = binding.telefonoInput.getText().toString().replaceAll("\n", "§");
+            String note = binding.noteInput.getText().toString().replaceAll("\n", "§");
+
+            this._oldValue = codice + "|" + nome + "|" + telefono + "|" + note + "|";
+        }
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            StorageManager storageManager = (StorageManager) getSystemService(STORAGE_SERVICE);
+            List<StorageVolume> storageVolumeList = storageManager.getStorageVolumes();
+            StorageVolume storageVolume = storageVolumeList.get(1); // 1 is for external SD Card. 0 is
+            _FILE = new File(storageVolume.getDirectory().getAbsolutePath() + "/" + _FILENAME);
         }
     }
 }
